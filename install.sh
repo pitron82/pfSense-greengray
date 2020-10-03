7cf695ee8901e55f2bb94f22993ce76c393d2d8d
pitron82 <65535766+pitron82@users.noreply.github.com>
2020-10-03 09:02:55 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,2 @@
+cp -r ./theme/pfSense-greengray.css /usr/local/www/css/
+cp -r ./theme/themes/* /usr/local/www/vendor/jquery-ui/themes/
---
+++ readme.md
@@ -0,0 +1,5 @@
+Theme for pfSense 2.4.5
+
+Run install.sh
+
+Go System, General Setup, webConfigurator Choose pfSense-anjelohe
