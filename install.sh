651cd1b6a74bbf7076d2acf081ed2f104068bd08
pitron82 <65535766+pitron82@users.noreply.github.com>
2025-01-31 16:45:30 +0100

    Delete install.sh

--- install.sh
+++
@@ -1,4 +0,0 @@
-pkg install git
-git clone https://github.com/pitron82/pfSense-greengrey.git
-cp -r ./pfSense-greengrey/theme/pfSense-greengrey.css /usr/local/www/css/
-cp -r ./pfSense-greengrey/theme/themes/* /usr/local/www/vendor/jquery-ui/themes/
