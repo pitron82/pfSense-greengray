6b91052ac5bd0f9f9feabfa63780d41ab620602e
pitron82 <65535766+pitron82@users.noreply.github.com>
2025-01-31 16:13:41 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 pkg install git
-git clone https://github.com/pitron82/pfSense-greengray.git
-cp -r ./pfSense-greengray/theme/pfSense-greengray.css /usr/local/www/css/
-cp -r ./pfSense-greengray/theme/themes/* /usr/local/www/vendor/jquery-ui/themes/
+git clone https://github.com/pitron82/pfSense-greengrey.git
+cp -r ./pfSense-greengrey/theme/pfSense-greengrey.css /usr/local/www/css/
+cp -r ./pfSense-greengrey/theme/themes/* /usr/local/www/vendor/jquery-ui/themes/
